9ba3fa6ba4b34a3f692eb40973cfdc187b2995e3
srithep444 <srithep555@gmail.com>
2018-09-24 10:13:24 +0700

    Import & Export Components

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
 
 export default class App extends React.Component {
-  doIt = () => {
-    console.log("Hello from console")
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text onPress={this.doIt}>Hello World</Text>
-      </View>
+      <Weather/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
