2d9ec3f0cd2a615a1a83fe0233d739e92cdbe3ea
srithep444 <srithep555@gmail.com>
2018-09-24 11:01:45 +0700

    Components and Image background

--- components/Weather.js
+++ components/Weather.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Forecast from './Forecast';
 
-export default class App extends React.Component {
-  doIt = () => {
-    console.log("Hello from console")
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{this.props.zipCode}</Text>
-      </View>
-    );
-  }
+export default class Weather extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            forecast: {
+                main: '-', description: '-', temp: 0
+            }
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={require('../assets/Peach(Edit)1.jpg')} style={styles.backdrop}>
+                    <Text>Zip code is {this.props.zipCode}.</Text>
+                    <Forecast {...this.state.forecast} /> 
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { paddingTop: 25  },
+  backdrop: { width: '100%', height: '100%'},
 });
