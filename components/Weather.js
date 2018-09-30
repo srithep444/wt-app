e3b6effa285fbe01e4c16fae699c8b28528e0f37
srithep444 <srithep555@gmail.com>
2018-10-01 02:43:08 +0700

    ใส่ AppID ดึงข้อมูลสภาพอากาศ

--- components/Weather.js
+++ components/Weather.js
@@ -12,6 +12,25 @@ export default class Weather extends React.Component {
         }
     }
 
+    fetchData = () => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+        .then((response) => response.json())
+        .then((json) => {
+            this.setState(
+        {
+        forecast: {
+            main: json.weather[0].main,
+            description: json.weather[0].description,
+            temp: json.main.temp
+        }
+        });
+        })
+        .catch((error) => {
+        console.warn(error);
+        });
+    }
+        componentDidMount = () => this.fetchData()
+
     render() {
         return (
             <View style={styles.container}>
